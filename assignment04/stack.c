d932eee18e6f75f3d1825185c3427abd8d404a4f
Aaron P Bloch <92547081+ApBloch@users.noreply.github.com>
2021-11-15 07:26:38 -0500

    Add assignment criteria to file

--- assignment04/stack.c
+++ assignment04/stack.c
@@ -1,2 +1,13 @@
 #includue "stack.h"
 
+/*
+Design Criteria:
+  i. The stack should have a predefined size 
+  ii. The stack supports “int” data types. 
+  iii. Provide a function to initialize the stack internals. 
+  iv. Provide a function to push an element onto the stack 
+  v. Provide a function to pop an element off the stack. 
+  vi. Provide a function that returns 1 if stack is empty. 
+  vii. Provide a function that returns 1 if stack is full. 
+  viii. Return (-1) for error conditions 
+*/
