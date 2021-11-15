25c184bc70ed5adeb800291c813f1444adacad9a
Aaron P Bloch <92547081+ApBloch@users.noreply.github.com>
2021-11-15 08:37:49 -0500

    Stack first try

--- assignment04/stack.h
+++ assignment04/stack.h
@@ -1,6 +1,14 @@
 #ifndef __STACK_H__
 #define __STACK_H__
 
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Stack;
+struct Stack* setupStack(int size);
+int isFull(struct Stack* stack);
+int isEmpty(struct Stack* stack);
+int push(struct Stack* stack, int value);
+int pop(struct Stack* stack);
 
 #endif
